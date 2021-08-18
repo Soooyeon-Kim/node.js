69caf20182961a9890717bcb2ccc4933fa648848
Soooyeon-Kim <kob04124@gmail.com>
2021-08-19 02:52:48 +0900

    로그인 요청 fetch 생성

---
+++ app/src/public/js/home/login.js
@@ -0,0 +1,26 @@
+"use strict";
+
+const id = document.querySelector("#id"),
+    psword = document.querySelector("#psword"),
+    loginBtn = document.querySelector("button");
+
+loginBtn.addEventListener("click", login);
+
+function login() {
+    const req = {
+        id : id.value,
+        psword : psword.value,
+
+    };
+
+
+    fetch("/login", {
+        method: "POST",
+        headers:{
+            "Contnet-Type":"application/json",
+        },
+        body: JSON.stringify(req)
+    })
+
+}
+

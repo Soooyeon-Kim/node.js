91a8d7bfeb6045ce086881e4e7e5de6e58396011
Soooyeon-Kim <kob04124@gmail.com>
2021-08-19 18:28:29 +0900

    fs모듈을 통한 회원가입 기능 구현

--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -2,7 +2,7 @@
 const fs = require("fs").promises;
 
 class UserStorage {
-    static #getUserInfo() {
+    static #getUserInfo(data, id) {
         const users = JSON.parse(data);
         const idx = users.id.indexOf(id);
         const userKeys = Object.keys(users); // => [id, psword, name]
@@ -14,7 +14,9 @@ class UserStorage {
         return userInfo;
     }
 
-    static getUsers(...fields) {
+    static #getUsers(data, isAll, fields){
+        const users = JSON.parse(data);
+        if (isAll) return users;
         // private 변수나 메서드는 항상 class 최상단에 위치시킨다.
         // const users = this.users;
         const newUsers = fields.reduce((newUsers, field) => {
@@ -26,6 +28,15 @@ class UserStorage {
         return newUsers;
     }
 
+    static getUsers(isAll, ...fields) {
+        return fs
+        .readFile("./src/databases/users.json")
+        .then((data) => {
+            return this.#getUsers(data, isAll, fields);
+        })
+        .catch(console.error);
+    }
+
     static getUserInfo(id) {
         return fs
         .readFile("./src/databases/users.json")
@@ -35,13 +46,16 @@ class UserStorage {
         .catch(console.error);
     }
 
-    
-
-    static save(userInfo) {
-        //const users = this.#users;
+    static async save(userInfo) {
+        const users = await this.getUsers(true);
+        if (users.id.includes(userInfo.id)) {
+            throw "이미 존재하는 아이디입니다.";
+        }
         users.id.push(userInfo.id);
         users.name.push(userInfo.name);
         users.psword.push(userInfo.psword);
+        // 데이터 추가 : writeFile (저장할 파일의 경로 , 저장할 데이터)
+        fs.writeFile("./src/databases/users.json", JSON.stringify(users));
         return { success: true };
     }
 }
--- app/src/public/js/home/register.js
+++ app/src/public/js/home/register.js
@@ -10,7 +10,8 @@ registerBtn.addEventListener("click", register);
 
 function register() {
     if (!id.value) return alert("아이디를 입력해주세요.");
-    if (psword.value !== confirmPsword.value) return alert('비밀번호가 일치하지 않습니다.');
+    if (psword.value !== confirmPsword.value) 
+        return alert('비밀번호가 일치하지 않습니다.');
     
 
     const req = {
@@ -37,7 +38,7 @@ function register() {
         }
       })
       .catch((err) => {
-          console.error(new Error("회원가입 중 에러 발생"));
+          console.error("회원가입 중 에러 발생");
       });
 }   
 

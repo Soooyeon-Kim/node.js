a0b017d156b033bcbecc6cd0b1f9b3bd077d683d
Soooyeon-Kim <kob04124@gmail.com>
2021-08-19 16:20:58 +0900

    fs모듈을 통한 로그인 기능 구현

--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -1,24 +1,9 @@
 "use strict"
+const fs = require("fs").promises;
 
 class UserStorage {
-    static #users = {
-        id: ["sooyeon" , "eunji" , "jisoo"],
-        psword: ["1234", "5678", "9090"],
-        name: ["김수연", "정은지", "강지수"],
-    };
-
-    static getUsers(...fields) {
-        const users = this.#users;
-        const newUsers = fields.reduce((newUsers, field) => {
-            if (users.hasOwnProperty(field)) {
-            newUsers[field] = users[field];
-            }
-            return newUsers;
-        }, {});
-        return newUsers;
-    }
-    static getUserInfo(id) {
-        const users = this.#users;
+    static #getUserInfo() {
+        const users = JSON.parse(data);
         const idx = users.id.indexOf(id);
         const userKeys = Object.keys(users); // => [id, psword, name]
         const userInfo = userKeys.reduce((newUser, info) => {
@@ -29,8 +14,31 @@ class UserStorage {
         return userInfo;
     }
 
+    static getUsers(...fields) {
+        // private 변수나 메서드는 항상 class 최상단에 위치시킨다.
+        // const users = this.users;
+        const newUsers = fields.reduce((newUsers, field) => {
+            if (users.hasOwnProperty(field)) {
+            newUsers[field] = users[field];
+            }
+            return newUsers;
+        }, {});
+        return newUsers;
+    }
+
+    static getUserInfo(id) {
+        return fs
+        .readFile("./src/databases/users.json")
+        .then((data) => {
+            return this.#getUserInfo(data, id);
+        })
+        .catch(console.error);
+    }
+
+    
+
     static save(userInfo) {
-        const users = this.#users;
+        //const users = this.#users;
         users.id.push(userInfo.id);
         users.name.push(userInfo.name);
         users.psword.push(userInfo.psword);
